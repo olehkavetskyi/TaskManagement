c418964461f47a2873a5d883319163bc0d5606c1
Oleh <olehkavetskyi@gmail.com>
2024-11-29 00:12:22 +0200

    Initialized .NET solution with layered architecture and project references

---
+++ src/Application/DependencyResolver.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Application;
+
+public static class DependencyResolver
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+
+        return services;
+    }
+}
